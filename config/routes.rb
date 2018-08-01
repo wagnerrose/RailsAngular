c265cb81e8de10e6b38ca582da78a51ce2149058
wagner rose <wagner_rose@yahoo.com.br>
2018-08-01 14:39:59 -0300

    atualizacao 01Ago18

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file,
+  # see http://guides.rubyonrails.org/routing.html
+  # usado para confiugrar subdominios
+  #  namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: "/" do
+    namespace :api, defaults: {format: :json} do
+
+    end
+
 end
