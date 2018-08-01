60b3e26075c30e410ef6d4b256c9036273626430
wagner rose <wagner_rose@yahoo.com.br>
2018-08-01 15:56:16 -0300

    Alteracao do arq de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
+require 'api_version_constraint'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file,
   # see http://guides.rubyonrails.org/routing.html
   # usado para confiugrar subdominios
   #  namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: "/" do
     namespace :api, defaults: {format: :json} do
-
+      namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
+        resources :tasks
+      end
     end
-
 end
