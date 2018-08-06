8b1b2d9e373902bbad69b76b21d6e872c5a92156
wagner rose <wagner_rose@yahoo.com.br>
2018-08-06 16:43:06 -0300

    Adding create action to the users controller with tests

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -29,4 +29,35 @@ RSpec.describe 'Users API', type: :request do
       end
     end
   end
+
+  describe  'POST /users' do
+    before do
+      headers = {'Accept' => 'application/vnd.taskmanager.v1'}
+      post '/users', params: {user: user_params}, headers: headers
+    end
+    context 'when the request params as valid' do
+      let(:user_params) {attributes_for(:user)}
+      # ou poderiamos      let(:user_params) {FactoryBot.attributes_for(:user)}
+      it 'return status code 201' do
+        expect(response).to have_http_status(201)
+      end
+      it 'return json user data' do
+        user_response = JSON.parse(response.body)
+        expect(user_response['email']).to eq(user_params[:email])
+      end
+    end
+
+    context 'when the request params are invalid' do
+      let(:user_params) {attributes_for(:user, email: 'invalid_email@')}
+
+      it 'return status code 422' do
+        expect(response).to have_http_status(422)
+      end
+      it 'return the json data for errors' do
+        user_response = JSON.parse(response.body)
+        expect(user_response).to have_key('errors')
+      end
+
+    end
+  end
 end
