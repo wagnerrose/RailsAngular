30ee8f7f185ce736abc988267e8eb67c5748bc48
wagner rose <wagner_rose@yahoo.com.br>
2018-08-06 12:39:51 -0300

    Adding new Users in controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ require 'api_version_constraint'
 
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file,
-  # see http://guides.rubyonrails.org/routing.html
+
   # usado para confiugrar subdominios
-  #  namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: "/" do
-    namespace :api, defaults: {format: :json} do
+    namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: "/" do
+   # namespace :api, defaults: {format: :json} do
       namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
-        resources :tasks
+        resources :users, only: [:show]
       end
     end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) {build(:user)}
+
   before { @user = FactoryBot.build(:user) }
 
-  it { expect(@user).to respond_to(:email)}
-  it { expect(@user).to respond_to(:name)}
-  it { expect(@user).to respond_to(:password)}
-  it { expect(@user).to respond_to(:password_confirmation)}
-  it { expect(@user).to be_valid }
+  #it { expect(@user).to respond_to(:email)}
+  it { expect(user).to respond_to(:email)}
+  #it { expect(@user).to respond_to(:name)}
+  #it { expect(@user).to respond_to(:password)}
+  #it { expect(@user).to respond_to(:password_confirmation)}
+  #it { expect(@user).to be_valid }
+
+  #it { expect(subject).to respond_to(:email)}
+
 end
