e213adf1baa1515b7219736e49f472e443e661b4
wagner rose <wagner_rose@yahoo.com.br>
2018-08-10 12:03:09 -0300

    atualizacao

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,15 +3,21 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) {build(:user)}
 
-  before { @user = FactoryBot.build(:user) }
+  #before { @user = FactoryBot.build(:user) }
 
-  #it { expect(@user).to respond_to(:email)}
-  it { expect(user).to respond_to(:email)}
-  #it { expect(@user).to respond_to(:name)}
-  #it { expect(@user).to respond_to(:password)}
-  #it { expect(@user).to respond_to(:password_confirmation)}
-  #it { expect(@user).to be_valid }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to validate_confirmation_of(:password) }
+  it { is_expected.to allow_value('costa@nonato.com').for(:email) }
+  it { is_expected.to validate_uniqueness_of(:auth_token) }
 
-  #it { expect(subject).to respond_to(:email)}
+describe "#info" do
+  it "returns email, created_at and token" do
+    user.save!
+    allow(Devise).to receive(:friendly_token).and_return('abc123zyzTOKEN')
+    expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: abc123zyzTOKEN")
+  end
+end
+
 
 end
