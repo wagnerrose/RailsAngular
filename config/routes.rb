1adadee2b58faf6ac4e520b7448a1fa00df1c3ee
Wagner Rose <wagner_rose@yahoo.com.br>
2018-08-21 09:37:22 -0300

    backup 20Ago18

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 require 'api_version_constraint'
 
 Rails.application.routes.draw do
-  #devise_for :users
+  devise_for :users, only: [:sessions], controllers: { sessions: 'api/v1/sessions'}
 
   # usado para confiugrar subdominios
     namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do
    # namespace :api, defaults: {format: :json} do
       namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
         resources :users, only: [:show, :create, :update, :destroy]
+        resources :sessions, only: [:create]
       end
     end
 end
---
+++ spec/requests/api/v1/sessions_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Sessions API', type: :request do
+  before {host! 'api.taskmanager.test'}
+  let!(:user) { create(:user) }
+  let(:headers) do
+    {
+      'Accept' => 'application/vnd.taskmanager.v1',
+      'Content-Type' => Mime[:json].to_s
+    }
+  end
+  describe 'POST /sessions' do
+    before do
+      post '/sessions', params: { session: credentials}.to_json, headers: headers
+    end
+    context 'when the credentials are correct' do
+      let (:credentials) {{email: user.email, password: '123456'}}
+
+      it 'return status 200' do
+        expect(response).to have_http_status(200)
+      end
+      it 'returns the json data for the user with auth token' do
+        user.reload
+        expect(json_body[:auth_token]).to eq(user.auth_token)
+      end
+    end
+  end
+end
